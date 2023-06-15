bedb393d668fabff33dc0150eb1bb23f13aad674
ZRoman87 <zrazhevskiy.roman@yandex.ru>
2023-06-15 09:35:48 +0300

    1st commit

---
+++ src/main/java/ru/hogwarts/school/SkyproHomework3HogwartsSchoolApplication.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproHomework3HogwartsSchoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkyproHomework3HogwartsSchoolApplication.class, args);
+    }
+
+}
